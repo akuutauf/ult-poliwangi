be79193640569c484c6b9790212e5c0e8e571dc0
akuutauf <taufikhidayatbwx09121@gmail.com>
2023-08-09 14:42:30 +0700

    Menambahkan Liviwire Formulir Pengajuan Dosen

--- app/Http/Controllers/FormDosenController.php
+++ app/Http/Controllers/FormDosenController.php
@@ -2,10 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Berkas;
-use App\Models\BerkasLayanan;
-use App\Models\Layanan;
-use App\Models\Prodi;
 use Illuminate\Http\Request;
 
 class FormDosenController extends Controller
@@ -27,13 +23,7 @@ class FormDosenController extends Controller
      */
     public function create()
     {
-        $data = [
-            'prodis' => Prodi::all(),
-            'layanans' => Layanan::with('divisi')->orderBy('id_divisi', 'asc')->get(),
-            'berkas_layanans' => Berkas::all(),
-        ];
-
-        return view('pages.client.formulir.dosen.form-dosen', $data);
+        return view('pages.client.formulir.dosen.form-dosen');
     }
 
     /**
